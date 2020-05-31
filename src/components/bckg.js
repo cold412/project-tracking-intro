2181d0a40ddcdfc45cbc2adb640e0fcdfbddb441
cold412 <cold412+gitlab@protonmail.com>
2020-05-31 20:16:06 +0200

    MainContainer Test

---
+++ src/components/bckg.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  background: palevioletred;
+  border-radius: 3px;
+  border: none;
+  color: white;
+`;
+
+export const MainCont = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
-import React from "react"
+import React from 'react'
+import {Button, MainCont} from '../components/bckg'
+import { GlobalStyle } from '../components/reset.js'
 
-export default function Home() {
-  return <div>Hello world!</div>
+
+const index = () => {
+  return (
+    <div>
+      <GlobalStyle />
+      <MainCont>
+        <Button>Test</Button>
+        <h1>Test</h1>
+      </MainCont>
+    </div>
+  )
 }
+
+export default index
